9d88ba9a0cc7785dd71f1fa0f7d54d5a89a3834c
iam-phenomenal <ayandiranibrahim@outlook.com>
2022-11-17 03:08:52 +0100

    Updated env

--- environ.c
+++ environ.c
@@ -1,7 +1,8 @@
 #include "shell.h"
 
 /**
- * _myenv - prints the current environment
+ * _myenv - entry
+ * Description: print current environment
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  * Return: Always 0
