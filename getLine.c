85e8f69ff9e5477ccbcf2354f3f0c4b10288f1e6
iam-phenomenal <ayandiranibrahim@outlook.com>
2022-11-17 03:15:24 +0100

    Buffer update

--- getLine.c
+++ getLine.c
@@ -1,7 +1,8 @@
 #include "shell.h"
 
 /**
- * input_buf - buffers chained commands
+ * input_buf - entry
+ * Description: buffers chained commands
  * @info: parameter struct
  * @buf: address of buffer
  * @len: address of len var
